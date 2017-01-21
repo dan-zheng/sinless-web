d2a9c078211a72b9c33bd6976a6c50b5e9fab3c3
Dan Zheng <zheng321@purdue.edu>
2017-01-21 09:40:17 -0500

    Add d3 and c3

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-
-    // Place JavaScript code here...
-
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            columns: [
+                ["limit", 3, 3, 3, 3, 3, 3],
+                ['swears', 2, 3, 5, 1, 3, 2]
+            ],
+            types: {
+                limit: 'line',
+                swears: 'area-spline' // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
+            },
+            colors: {
+                limit: '#d62728',
+                swears: '#ff7f0e'
+            },
+        }
+    });
 });
